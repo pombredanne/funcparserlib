9097cd04f98942077eb35578aebbe7177221a34f
magniff <tinysnippets@gmail.com>
2016-02-26 20:17:36 -0500

    Start adding very basic tests, hmm, maybe I should give a try to hypotesis? Should I?)

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,21 @@
+import pytest
 from myparserlib import parser as p
 
 
-def test_parser():
-    assert True
+def test_simple_match():
+    a_matcher = p.a('a')
+
+    assert 'a' == a_matcher.parse('a')
+    with pytest.raises(p.NoParseError):
+        a_matcher.parse('v')
+
+
+def test_some_match():
+    digit_matcher = p.some(lambda item: str.isdigit(item))
+    for digit in range(10):
+        assert str(digit) == digit_matcher.parse(str(digit))
+
+
+def test_many_match():
+    digit_matcher = p.some(lambda item: str.isdigit(item))
+    assert ['1', '2', '3'] == p.many(digit_matcher).parse("123")
